3eaf72fb311da0bf74080d2211fd16201923004c
brais <bvg18@alu.ua.com>
2021-04-19 11:56:55 +0200

    inicio borrar wishlist

--- app/Http/Controllers/WishlistController.php
+++ app/Http/Controllers/WishlistController.php
@@ -54,4 +54,32 @@ class WishlistController extends Controller
         return redirect()->action('WishlistController@listWishlist', [$userId]);
     }
 
+    public function askAndDeleteWishlist($idWishlistDelete, $idWishlistProducts, $choose) 
+    {
+        $wishlistDelete = Wishlist::find($idWishlistDelete);
+        $wishlistProducts = Wishlist::find($idWishlistProducts);
+        $userId = Auth::id();$userId = Auth::id();
+
+        if ($choose == 1) {
+            // pasar productos y borrrar wishlist
+            // pasar productos
+            deleteWishlist($idWishlistDelete);
+        }
+        else {
+            // borrar todo 
+            deleteWishlist($idWishlistDelete);
+        }
+
+        return redirect()->action('WishlistController@listWishlist', [$userId]);  
+    }
+
+    public function deleteWishlist($id) 
+    {
+        $wishlist = Wishlist::find($id);
+        $userId = Auth::id();
+
+        $wishlist->delete();
+        return redirect()->action('WishlistController@listWishlist', [$userId]);  
+    }
+
 }
