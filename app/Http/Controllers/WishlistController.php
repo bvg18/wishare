412dcbfc455afe01903b8963a53a26886a4bdb3d
lajr1 <lajr1@alu.ua.es>
2021-04-27 17:23:28 +0200

    Pequeño cambio controlador

--- app/Http/Controllers/WishlistController.php
+++ app/Http/Controllers/WishlistController.php
@@ -77,7 +77,8 @@ class WishlistController extends Controller
 
         $categories = Category::All();
 
-        return view('wishlists/wishlist', ['wishlist' => $wishlist, 'products' => $products, 'categories' => $categories]);
+        $myList = (Auth::id() == $wishlist->user->id);
+        return view('wishlists/wishlist', ['wishlist' => $wishlist, 'products' => $products, 'categories' => $categories, 'myList'=>$myList]);
     }
 
     public function askWishlistChooseGET($idWishlistDelete) 
