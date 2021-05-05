85a543a862c701f8ffa714a33bded101ebbbc4d0
carloscaceresher <cch17@alu.ua.es>
2021-05-05 20:38:38 +0200

    casi vista about

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -12,13 +12,38 @@
         height: 100%;
         width: 100%;
         background: rgb(202,207,214);
-background: linear-gradient(90deg, rgba(202,207,214,1) 0%, rgba(214,229,227,1) 27%, rgba(159,216,203,1) 100%);
+        background: linear-gradient(90deg, rgba(202,207,214,1) 0%, rgba(214,229,227,1) 27%, rgba(159,216,203,1) 100%);
+        background-position: fixed;
     }
     #texto-hero{
         position:absolute;
         top:50%;
         left:50%;
         transform: translate(-50%,-50%);
+    }
+    #seccion2{
+        margin-top: 5em;
+        margin-bottom: 5em;
+    }
+    #seccion3{
+        border-radius:10px;
+        margin-bottom: 5em;
+    }
+    #seccion4{
+        margin-top: 7em;
+        margin-bottom: 5em;
+    }
+    .answer{
+        transition:all .1s ease;
+        border:none;
+    }
+    .answer:hover{
+        background-color: #d6e5e3;
+        text-decoration: none;
+    }
+    .btn-verde{
+        background-color:#9fd8cb;
+        color:white;
     }
 </style>
 
@@ -34,7 +59,7 @@ background: linear-gradient(90deg, rgba(202,207,214,1) 0%, rgba(214,229,227,1) 2
     </div>
 </div>
 
-<div id="seccion2" class="container my-5">
+<div id="seccion2" class="container">
     <div class="row mb-5">
         <div class="col">
             <h2>Titulo de prueba</h2>
@@ -49,12 +74,35 @@ background: linear-gradient(90deg, rgba(202,207,214,1) 0%, rgba(214,229,227,1) 2
         </div>
     </div>
 </div>
+<div class="container">
+    <div id="seccion3" class=" my-5 py-5 shadow">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-12 col-lg-4">
+                    <img class="img-fluid" src="/img/about/undraw_Note_list_re_r4u9.png" alt="">
+                </div>
+                <div class="col-12 col-lg-8">
+                    <h2>titulo 1</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati autem velit earum ullam vitae nisi? Tenetur, numquam qui eos tempore velit delectus unde nostrum est ducimus, laudantium repudiandae facilis. A!</p>
+                    <a href="" class="btn btn-block btn-verde mt-1 d-block d-lg-none">Llamada a la accion</a>
+                    <a href="" class="btn btn-verde mt-1 d-none d-lg-inline-flex">Llamada a la accion</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
-<div id="seccion3" class="container my-5 shadow">
-    <div class="row justify-content-center">
-        <div class="col-12 col-lg-8">
-            <h2>titulo 1</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati autem velit earum ullam vitae nisi? Tenetur, numquam qui eos tempore velit delectus unde nostrum est ducimus, laudantium repudiandae facilis. A!</p>
+<div id="seccion4" class="container">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-12 mb-2">
+                <h3 class="text-center">FAQ's</h3>
+            </div>
+            <div class="col-12 col-lg-8">
+                <a href="#multiCollapseExample1" role="button" aria-expanded="false" aria-controls="multiCollapseExample1" class="card shadow p-3 answer">
+                    <h4 class="text-center text-dark">¿Pregunta 1?</h4>
+                </a>
+            </div>
         </div>
     </div>
 </div>
